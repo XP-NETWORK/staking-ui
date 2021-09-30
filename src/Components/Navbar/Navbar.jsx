0794546ac6831c040e5d5ca34f74719f47521877
yourxpnetwork <yurad@xp.network>
2021-09-30 16:11:34 +0300

    routers toggle styled

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,9 +5,11 @@ import { Link } from "react-router-dom";
 import { connectMetaMask } from "../../utils/metamask"
 import { useSelector, useDispatch } from 'react-redux'
 import { chengeStatus } from "../../redux/counterSlice";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Navbar() {
+
+    const [rendered, setRendered] = useState('stake')
 
     const metaMaskStatus = useSelector(state => state.data.connected)
     const dispatch = useDispatch()
@@ -24,8 +26,8 @@ export default function Navbar() {
         <div className="navbar">
             <div className="nav">
                 <div className="xp__logo"><img src={xplogo} alt="XP.Network Logo" /></div>
-                <div className="Stake nav__button nav__button--active"><Link to='/stake'>Stake XPNET</Link></div>
-                <div className="Claim nav__button"><Link to='/claim'>Claim XPNET</Link></div>
+                <div className="nav__buttons"><div onClick={() => setRendered('stake')} className={rendered !== 'stake' ?`Stake nav__button`:`Stake nav__button--active`}><Link to='/stake'>Stake XPNET</Link></div>
+                <div onClick={() => setRendered('claim')} className={rendered !== 'claim' ?`Claim nav__button`:`Claim nav__button--active`}><Link to='/claim'>Claim XPNET</Link></div></div>
             </div>
             <div onClick={ () => toggleMetaMask()} className="metamask">
                 <div className="metamask__status"><div className={metaMaskStatus ? 'online' : 'offline'}></div></div>
