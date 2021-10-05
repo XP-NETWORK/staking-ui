7475e5cecf0d5ac4bb3a0e966b9aa758b3ea8856
yourxpnetwork <yurad@xp.network>
2021-10-05 14:43:27 +0300

    slice added to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counterSlice'
+import stakeSlice from './stakeSlice'
+
 
 export const store = configureStore({
     reducer: {
       data: counterSlice,
+      stakeData: stakeSlice
     },
   })
