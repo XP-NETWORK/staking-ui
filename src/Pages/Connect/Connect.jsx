ebdda969add5a74e3c88c82834a910821bdfcb90
yourxpnetwork <yurad@xp.network>
2021-10-06 15:08:01 +0300

    metamask connect page

--- src/Pages/Connect/Connect.jsx
+++ src/Pages/Connect/Connect.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./Connect.css"
+import MetaMask from "../../assets/MetaMask_Big_Fox.svg"
 
 export default function Connect() {
     return (
-        <div>
-            Connect
+        <div className="connect__container">
+            <div className="connect">
+                <div className="fox">
+                    <img src={MetaMask} alt="" />
+                </div>
+                <div className="connect__title">
+                    Connect to MetaMask
+                </div>
+                <div className="connect__button">
+                    Connect
+                </div>
+            </div>
         </div>
     )
 }
