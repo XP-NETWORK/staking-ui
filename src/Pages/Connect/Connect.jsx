432ab50d5806dea82221b4e543446d095cbd148b
yourxpnetwork <yurad@xp.network>
2021-10-06 14:43:08 +0300

    metamask connect page

---
+++ src/Pages/Connect/Connect.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Connect.css"
+
+export default function Connect() {
+    return (
+        <div>
+            Connect
+        </div>
+    )
+}
--- src/Pages/Stake/Stake.jsx
+++ src/Pages/Stake/Stake.jsx
@@ -13,6 +13,7 @@ import { getPercent } from "../../utils/helper"
 import { stake } from "../../utils/stake"
 import { approve } from "../../utils/xpnet"
 import { Approvance, Lock} from "../../Components/Buttons/Buttons"
+import Connect from '../Connect/Connect'
 
 
 export default function Stake() {
@@ -92,95 +93,99 @@ useEffect(() => {
 useEffect(() => {
 }, [allowence])
 
-    return (
-        <div className="stake__container">
-            <div className="stake">
-                <div className="stake__title">My Stake</div>
-                <div className="line"></div>
-                <div className="stake__duration">
-                    <div className="duration__header">
-                        Staking Duration
+    if(account){
+        return (
+            <div className="stake__container">
+                <div className="stake">
+                    <div className="stake__title">My Stake</div>
+                    <div className="line"></div>
+                    <div className="stake__duration">
+                        <div className="duration__header">
+                            Staking Duration
+                        </div>
+                        <div className="durations__container">
+                            {durations.map((e,i) => {
+                               return <Duration month={e.d} apy={e.p} key={i} index={i} />
+                            })}
+                        </div>
                     </div>
-                    <div className="durations__container">
-                        {durations.map((e,i) => {
-                           return <Duration month={e.d} apy={e.p} key={i} index={i} />
-                        })}
+                    <div className="stake__amount">
+                        <div className="amount__header">
+                            <div className="amount__title">Enter your XPNET amount</div>
+                            <div className="amount__subtitle">Availabe for Staking: <span>{balance} XPNET</span></div>
+                        </div>
+                        <div className="amount__input">
+                            <input value={amount} onChange={item => amountHandler(item)} onBlur={item => onBlurHandler(item)} type="text" />
+                            <div className="input__items">
+                                <div className="xpnet">XPNET</div>
+                                <div onClick={ () => putMax()} className="max">MAX</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="stake__amount">
-                    <div className="amount__header">
-                        <div className="amount__title">Enter your XPNET amount</div>
-                        <div className="amount__subtitle">Availabe for Staking: <span>{balance} XPNET</span></div>
+                    <div className="stake__rewards">
+                        <div className="rewards__header">
+                            <div className="rewards__title">Staking Rewards</div>
+                            <div className="rewards__percent">{getPercent(durations, duration)}%</div>
+                        </div>
+                        <div className="rewards">
+                            <div className="rewards__widget">
+                                <img src={image} alt="Reward art" />
+                            </div>
+                            <div className="rewards__content">
+                                <div className="rewards__subtitle">Don't wait 3 month - get your NFT Reward right NOW</div>
+                                <div className="rewards__text">It is a long established fact that a reader will be content of a page when.</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="amount__input">
-                        <input value={amount} onChange={item => amountHandler(item)} onBlur={item => onBlurHandler(item)} type="text" />
-                        <div className="input__items">
-                            <div className="xpnet">XPNET</div>
-                            <div onClick={ () => putMax()} className="max">MAX</div>
+                    <div className="stake__info">
+                        <div className="info__title">
+                            <img src={i} alt="" />
+                            <span>It is a long</span>
+                        </div>
+                        <div className="info__text">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
                         </div>
                     </div>
                 </div>
-                <div className="stake__rewards">
-                    <div className="rewards__header">
-                        <div className="rewards__title">Staking Rewards</div>
-                        <div className="rewards__percent">{getPercent(durations, duration)}%</div>
-                    </div>
-                    <div className="rewards">
-                        <div className="rewards__widget">
-                            <img src={image} alt="Reward art" />
+                <div className="summary">
+                    <div className="stake__title">Summary</div>
+                    <div className="line"></div>
+                    <div className="summary__details">
+                        <div className="details details__amount">
+                            <div className="details__capture">Staking Amount</div>
+                            <div className="details__text">{amount} XPNET<span>$ {(amount*currentPrice).toFixed(2)}</span></div>
                         </div>
-                        <div className="rewards__content">
-                            <div className="rewards__subtitle">Don't wait 3 month - get your NFT Reward right NOW</div>
-                            <div className="rewards__text">It is a long established fact that a reader will be content of a page when.</div>
+                        <div className="details details__start">
+                            <div className="details__capture">Start Date</div>
+                            <div className="details__text">{startDate}</div>
                         </div>
-                    </div>
-                </div>
-                <div className="stake__info">
-                    <div className="info__title">
-                        <img src={i} alt="" />
-                        <span>It is a long</span>
-                    </div>
-                    <div className="info__text">
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                        <div className="details details__end">
+                            <div className="details__capture">End Date</div>
+                            <div className="details__text">{endDate}</div>
+                        </div>
+                        <div className="line"></div>
+                        <div className="details details__apy">
+                            <div className="details__capture">Est. APY</div>
+                            <div className="details__text">{getPercent(durations, duration)}%</div>
+                        </div>
+                        <div className="details details__rewards">
+                            <div className="details__capture">Estimated APY</div>
+                            <div className="details__text">{getRewards()} XPNET<span>$ {(getRewards()*currentPrice).toFixed(2)}</span></div>
+                        </div>
+                        <div className="line"></div>
+                        <div className="agreement">
+                         <input onChange={() => agreementHandler()} checked={agreement} type="checkbox" name="agree" id="agree" />
+                         <div className="agreement__text">
+                          I have read and I agree to <a href="#">XPNET Staking Service Agreement</a>
+                         </div>
+                        </div>
+                        <Approvance agreement={agreement} approvance={allowence} amount={amount} duration={duration} account={account} />
+                        <Lock agreement={agreement} approvance={allowence} amount={amount} duration={duration} account={account} />
                     </div>
                 </div>
             </div>
-            <div className="summary">
-                <div className="stake__title">Summary</div>
-                <div className="line"></div>
-                <div className="summary__details">
-                    <div className="details details__amount">
-                        <div className="details__capture">Staking Amount</div>
-                        <div className="details__text">{amount} XPNET<span>$ {(amount*currentPrice).toFixed(2)}</span></div>
-                    </div>
-                    <div className="details details__start">
-                        <div className="details__capture">Start Date</div>
-                        <div className="details__text">{startDate}</div>
-                    </div>
-                    <div className="details details__end">
-                        <div className="details__capture">End Date</div>
-                        <div className="details__text">{endDate}</div>
-                    </div>
-                    <div className="line"></div>
-                    <div className="details details__apy">
-                        <div className="details__capture">Est. APY</div>
-                        <div className="details__text">{getPercent(durations, duration)}%</div>
-                    </div>
-                    <div className="details details__rewards">
-                        <div className="details__capture">Estimated APY</div>
-                        <div className="details__text">{getRewards()} XPNET<span>$ {(getRewards()*currentPrice).toFixed(2)}</span></div>
-                    </div>
-                    <div className="line"></div>
-                    <div className="agreement">
-                     <input onChange={() => agreementHandler()} checked={agreement} type="checkbox" name="agree" id="agree" />
-                     <div className="agreement__text">
-                      I have read and I agree to <a href="#">XPNET Staking Service Agreement</a>
-                     </div>
-                    </div>
-                    <Approvance agreement={agreement} approvance={allowence} amount={amount} duration={duration} account={account} />
-                    <Lock agreement={agreement} approvance={allowence} amount={amount} duration={duration} account={account} />
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }else{
+        return <Connect />
+    }
 }
