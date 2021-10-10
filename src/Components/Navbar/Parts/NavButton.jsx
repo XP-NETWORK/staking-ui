2cbde4799bfe0ea35ddb5afc5ec179bdca85e201
yourxpnetwork <yurad@xp.network>
2021-10-10 10:02:51 +0300

    navbar claim button disabled

--- src/Components/Navbar/Parts/NavButton.jsx
+++ src/Components/Navbar/Parts/NavButton.jsx
@@ -4,7 +4,22 @@ import { Link } from "react-router-dom";
 
 export default function NavButton({ location, type, balance }) {
     if(type === 'stake-btn') return (
-        <div className={location.pathname === '/stake' || location.pathname === '/' ? `Stake nav__button--active`: `Stake nav__button`}><Link to='/stake'>Stake XPNET</Link></div>
+        <div className={location.pathname === '/stake' || location.pathname === '/' ? `Stake nav__button--active`: `Stake nav__button`}>
+            <Link to='/stake'>Stake XPNET</Link>
+        </div>
     )
-    else if(type === 'claim-btn') return <div style={{visibility:`${balance ? 'visible' : 'hidden'}`}} className={location.pathname === '/claim' ?`Claim nav__button--active`:`Claim nav__button`}><Link to='/claim'>Claim XPNET</Link></div>
+    else if(type === 'claim-btn') {
+        if(balance){
+            return (
+                <div className={location.pathname === '/claim' ?`nav__button--active`:`nav__button`}>
+                    <Link to='/claim'>Claim XPNET</Link>
+                </div>)
+        }
+        else {
+            return ( 
+                <div className="nav__button--disabled">
+                    Claim XPNET
+                </div> )
+        }
+    }
 }
