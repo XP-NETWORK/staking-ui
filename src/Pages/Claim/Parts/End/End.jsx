c8f20e7dfb855839ccef0053c4a7ea2834471abd
yourxpnetwork <yurad@xp.network>
2021-10-18 13:35:18 +0300

    input error

--- src/Pages/Claim/Parts/End/End.jsx
+++ src/Pages/Claim/Parts/End/End.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { getStartDate, getEndDate } from "../../../../utils/helper"
+import ButtonLoader from '../../../../Components/Loader/ButtonLoader'
 
 export default function End({ startTime, period, startDate }) {
     return (
     <div className="claim__det claim__end">
         <div className="claim__capture">End day</div>
-        <div className="claim__text">{getStartDate(startTime) !== "Invalid date" ? getEndDate(period, startDate): "--:--:-- --:--"}</div>
+        <div className="claim__text">{getStartDate(startTime) !== "Invalid date" ? getEndDate(period, startDate): <ButtonLoader />}</div>
     </div>
     )
 }
