221d198fa440ac32038e3d3a4b9e29a25fd2ca15
yourxpnetwork <yurad@xp.network>
2021-10-20 17:20:01 +0300

    account icon fixed

--- src/Components/Navbar/MetaMask.jsx
+++ src/Components/Navbar/MetaMask.jsx
@@ -9,20 +9,24 @@ export default function MetaMask() {
     const account = useSelector(state => state.data.account)
 
     useEffect(() => {
-        let Icon = createIcon({ // All options are optional
-            seed: account, // seed used to generate icon data, default: random
-            color: '#dfe', // to manually specify the icon color, default: random
-            bgcolor: '#aaa', // choose a different background color, default: white
-            size: 15, // width/height of the icon in blocks, default: 10
-            scale: 3 // width/height of each block in pixels, default: 5
-        });
-        const l = document.getElementById('ls')
-        
-        if(l){
-            l.innerHTML = ''
-            l.appendChild(Icon)
+
+        const list = document.querySelectorAll('.account__icon')
+        console.log(list)
+        if(list){
+            list.forEach((item, index) => {
+                let Icon = createIcon({ // All options are optional
+                    seed: account, // seed used to generate icon data, default: random
+                    color: '#dfe', // to manually specify the icon color, default: random
+                    bgcolor: '#aaa', // choose a different background color, default: white
+                    size: 15, // width/height of the icon in blocks, default: 10
+                    scale: 3 // width/height of each block in pixels, default: 5
+                });
+                item.innerHTML = ''
+                item.appendChild(Icon)
+                console.log(item, index)
+            })
         }
-        }, [])
+    }, [])
 
 
     return (
@@ -30,7 +34,7 @@ export default function MetaMask() {
             <div className="account">
                 {account.slice(0,6) + '...' + account.slice(38,46)}
             </div>
-           <div id="ls" className="account__icon"></div>
+           <div className="account__icon"></div>
         </div>
     )
 }
