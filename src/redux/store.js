b51c4978fb577638b6b339bb96b409da71a5caa9
yuraD <yurad@xp.network>
2021-10-28 15:19:22 +0300

    totalSupplay redux slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counterSlice'
 import stakeSlice from './stakeSlice'
+import totalSupply from './totalSupply'
 
 
 export const store = configureStore({
     reducer: {
       data: counterSlice,
-      stakeData: stakeSlice
+      stakeData: stakeSlice,
+      totalSupply
     },
   })
