3e254da5bf27b605295fd028b3e021fe630ddc12
yuraD <yurad@xp.network>
2021-11-18 14:07:59 +0200

    progress bar days remaining

--- src/Pages/Claim/Parts/ProgressBar/ProgressBar.jsx
+++ src/Pages/Claim/Parts/ProgressBar/ProgressBar.jsx
@@ -1,5 +1,5 @@
 import moment from 'moment'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { getProgress } from "../../../../utils/helper"
 
@@ -9,9 +9,13 @@ export default function ProgressBar({ period, startTime }) {
 
     const [toDay, setToDay] = useState(moment().unix())
     const startDayUnix = useSelector(state => state.stakeData.startTime)
-    const [remain, setRemain] = useState(Math.round((period/60/60/24) - ((toDay - startDayUnix)/60/60/24)))
+    const [remain, setRemain] = useState()
 
-    // Math.round((period/60/60/24) - ((toDay - startDayUnix)/60/60/24))
+    useEffect(() => {
+        const num = Math.round(((period/60/60/24) - (toDay - startDayUnix)/60/60/24))
+        setRemain(num)
+
+    }, [])
 
     return (
     <div className="progress-bar">
