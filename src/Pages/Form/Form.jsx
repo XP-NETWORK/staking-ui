dfb0dee75e862eb986644159fa94c4fb3c06d5be
Yura Dukhno <yura@Yuras-MacBook-Pro.local>
2022-01-17 10:45:18 +0200

    post request

--- src/Pages/Form/Form.jsx
+++ src/Pages/Form/Form.jsx
@@ -1,42 +1,68 @@
 import Modal from 'react-modal';
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import "./Form.css"
 import { setNotifyForm } from '../../redux/counterSlice';
+import axios from 'axios';
 
 export default function Form() {
     const dispatch = useDispatch()
     const notifyForm = useSelector(state => state.data.notifyForm)
-
+    const [firstName, setFirstName] = useState()
+    const [lastName, setLastName] = useState()
+    const [mail, setMail] = useState()
+    const [errMail, setErrMail] = useState(false)
+    const [telegram, setTelegram] = useState()
+    const hidden = {visibility: "hidden"}
+    // const pattern = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
     const handleClose = () => {
         dispatch(setNotifyForm(false))
     }
     
     const customStyles = {
-        // content: {
-        //   top: '45%',
-        //   left: '50%',
-        //   right: 'auto',
-        //   bottom: 'auto',
-        //   marginRight: '-50%',
-        //   transform: 'translate(-50%, -50%)',
-        //   outline: 'none'
-        // },
         overlay: {
           background: "rgba(0, 0, 0, 0.5)"
         }
-      };
+    };
 
+
+
+    const onSubmitHandler = e => {
+        debugger
+        const pattern = new RegExp(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
+        e.preventDefault()
+        let msg
+        if(pattern.test(mail.toLowerCase())){
+            msg = {
+                firstName,
+                lastName,
+                mail,
+                telegram
+            }
+            axios
+            .post("#", JSON.stringify(msg))
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
+            setErrMail(false)
+        }
+        else{
+            setErrMail(true)
+            setMail('')
+        }
+    }
 
     return (
         <Modal onRequestClose={handleClose} isOpen={notifyForm} className="notify-modal" style={customStyles}>
             <div onClick={handleClose} className='notify-modal-close'>&#x2715;</div>
             <div className='notify-form__title'>Stay tuned</div>
-            <form>
-                <input className="notify-input" type="text" name="name" placeholder='First Name*' />
-                <input className="notify-input" type="text" name="name" placeholder='Last Name*' />
-                <input className="notify-input" type="text" name="name" placeholder='Email*' />
-                <input className="notify-input" type="text" name="name" placeholder='Telegram*' />
+            <form onSubmit={e => onSubmitHandler(e)}>
+                <input value={firstName} onChange={e => setFirstName(e.target.value)} className="notify-input" type="text" name="name" placeholder='First Name*' />
+                <input value={lastName} onChange={e => setLastName(e.target.value)} className="notify-input" type="text" name="name" placeholder='Last Name*' />
+                <div className={`notify-input`}>
+                    <input style={{border: "none"}} value={mail} onChange={e => setMail(e.target.value)}type="mail" name="name" placeholder='Email*' />
+                    <span style={!errMail ? hidden : {}} className='mail--invalid'>Invalid mail input</span>
+                </div>
+                <input value={telegram} onChange={e => setTelegram(e.target.value)} className="notify-input" type="text" name="name" placeholder='Telegram*' />
                 <input className="notify-submit" type="submit" value="Submit" />
             </form>
         </Modal>
