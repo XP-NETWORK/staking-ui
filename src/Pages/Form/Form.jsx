5e7171185e86370e34c7621f752180c570551608
Yura Dukhno <yura@Yuras-MacBook-Pro.local>
2022-01-17 15:06:44 +0200

    submit button unclickable on sending

--- src/Pages/Form/Form.jsx
+++ src/Pages/Form/Form.jsx
@@ -8,12 +8,13 @@ import axios from 'axios';
 
 export default function Form() {
     const dispatch = useDispatch()
+    const [sending, setSending] = useState(false)
     const notifyForm = useSelector(state => state.data.notifyForm)
     const [firstName, setFirstName] = useState('')
     const [validFirst, setValidFirst] = useState()
     const [lastName, setLastName] = useState('')
     const [validLast, setValidLast] = useState()
-    const [mail, setMail] = useState('')
+    const [email, setMail] = useState('')
     const [validMail, setValidMail] = useState()
     const [telegram, setTelegram] = useState('')
     const [validTelegram, setValidTelegram] = useState()
@@ -29,36 +30,79 @@ export default function Form() {
         }
     };
 
+    const handleFirstName = first => {
+        setFirstName(first)
+        if(first.length < 1 ){
+            setValidFirst("invalid")
+        }
+        else{
+            setValidFirst(true)
+        }
+    }
+
+    const handleLastName = last => {
+        setLastName(last)
+        if(last.length < 1 ){
+            setValidLast("invalid")
+        }
+        else{
+            setValidLast(true)
+        }
+    }
+
+    const handleTelegram = telegram => {
+        setTelegram(telegram)
+        if(telegram.length < 1 ){
+            setValidTelegram("invalid")
+        }
+        else{
+            setValidTelegram(true)
+        }
+    }
+
+    const handleMail = mail => {
+        const pattern = new RegExp(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
+        setMail(mail)
+        if(mail.length < 1 && pattern.test(mail)){
+            setValidMail("invalid")
+        }
+        else{
+            setValidMail(true)
+        }
+    }
 
 
     const onSubmitHandler = async(e) => {
         debugger
+        setSending(true)
         e.preventDefault()
         const pattern = new RegExp(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
-        firstName?.length > 1 ? setValidFirst(true) : setValidFirst('invalid')
-        lastName?.length > 1 ? setValidLast(true) : setValidLast('invalid')
-        telegram?.length > 1 ? setValidTelegram(true) : setValidTelegram('invalid')
-        if(mail?.length > 1 && pattern.test(mail?.toLowerCase())){
+        firstName?.length > 0 ? setValidFirst(true) : setValidFirst('invalid')
+        lastName?.length > 0 ? setValidLast(true) : setValidLast('invalid')
+        telegram?.length > 0 ? setValidTelegram(true) : setValidTelegram('invalid')
+        if(email?.length > 0 && pattern.test(email?.toLowerCase())){
             setValidMail(true)
         } 
         else{
             setValidMail('invalid')
         }
         let msg
-        if(validMail && validFirst && validLast && validTelegram && validMail){
+        if((validMail !== "invalid" ) && validFirst && validLast && validTelegram && validMail){
             msg = {
+                email,
+                telegram,
                 firstName,
                 lastName,
-                mail,
-                telegram
             }
             try {
-                const response = await axios.post("https://xpnetworkapi.herokuapp.com/stake-notify", JSON.stringify(msg))
+                const response = await axios.post("https://xpnetworkapi.herokuapp.com/stake-notify", msg)
                 setMsgSent(true)
             } catch (error) {
                 console.error(error);
+                setSending(false)
             }
         }
+        else setSending(false)
     }
 
     return (
@@ -78,22 +122,22 @@ export default function Form() {
                     <div className='notify-form__title'>Stay tuned</div>
                     <form onSubmit={e => onSubmitHandler(e)}>
                         <div className="notify-input">
-                            <input value={firstName} onChange={e => setFirstName(e.target.value)}  type="text" name="name" placeholder='First Name*' />
-                            <span style={validFirst === 'invalid' ? {} : hidden} className='invalid'>Invalid FirstName input</span>
+                            <input value={firstName} onChange={e => handleFirstName(e.target.value)}  type="text" name="name" placeholder='First Name*' />
+                            <span style={validFirst === 'invalid' ? {} : hidden} className='invalid'>Invalid First Name input</span>
                         </div>
                         <div className="notify-input">
-                            <input value={lastName} onChange={e => setLastName(e.target.value)} type="text" name="name" placeholder='Last Name*' />
-                            <span style={validLast === 'invalid'? {} : hidden} className='invalid'>Invalid LastName input</span>
+                            <input value={lastName} onChange={e => handleLastName(e.target.value)} type="text" name="name" placeholder='Last Name*' />
+                            <span style={validLast === 'invalid'? {} : hidden} className='invalid'>Invalid Last Name input</span>
                         </div>
                         <div className="notify-input">
-                            <input style={{border: "none"}} value={mail} onChange={e => setMail(e.target.value)}type="mail" name="name" placeholder='Email*' />
+                            <input style={{border: "none"}} value={email} onChange={e => handleMail(e.target.value)}type="mail" name="name" placeholder='Email*' />
                             <span style={validMail === 'invalid' ? {} : hidden} className='invalid'>Invalid mail input</span>
                         </div>
                         <div className="notify-input">
-                            <input value={telegram} onChange={e => setTelegram(e.target.value)} type="text" name="name" placeholder='Telegram*' />
+                            <input value={telegram} onChange={e => handleTelegram(e.target.value)} type="text" name="name" placeholder='Telegram*' />
                             <span style={validTelegram === 'invalid' ? {} : hidden} className='invalid'>Invalid Telegram input</span>
                         </div>
-                        <input className="notify-submit" type="submit" value="Submit" />
+                        <input className={sending ? "notify-submit--unclickable" : "notify-submit"} type="submit" value="Submit" />
                     </form>
                 </> 
             }
